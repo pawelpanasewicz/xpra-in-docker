355f0f9e0431feea69c56e1b282d7fc976522c11
pawel.panasewicz <pawel.panasewicz@gmail.com>
2017-02-18 18:07:47 +0100

    replace ;; with REM so intellij displays it nicely

--- 1-docker-rebuild.bat
+++ 1-docker-rebuild.bat
@@ -1,55 +1,55 @@
-;; Run it from windows CMD
+REM Run it from windows CMD
 
 docker build -t devbox .
 
 
-;; Output should be something like this:
+REM Output should be something like this:
 
-;; d:\devstation-workspace\projects\docker-devstation>docker build -t devbox .
-;; Sending build context to Docker daemon 77.31 kB
-;; Step 1/15 : FROM ubuntu:14.04
-;;  ---> b969ab9f929b
-;; Step 2/15 : MAINTAINER Pawel Panasewicz
-;;  ---> Using cache
-;;  ---> db5bfaab0e64
-;; Step 3/15 : RUN apt-get update -y  && apt-get upgrade -y  && apt-get install -y  apt-transport-https  curl  wget  telnet  vim  openssh-server  npm  firefox  sudo
-;;  ---> Using cache
-;;  ---> 644e239ec4fd
-;; Step 4/15 : RUN curl https://xpra.org/gpg.asc | apt-key add -
-;;  ---> Using cache
-;;  ---> fc011981f85c
-;; Step 5/15 : RUN echo "deb https://xpra.org/ trusty main" > /etc/apt/sources.list.d/xpra.list
-;;  ---> Using cache
-;;  ---> 571bc6da050f
-;; Step 6/15 : RUN apt-get update -y
-;;  ---> Using cache
-;;  ---> e3c3b061a492
-;; Step 7/15 : RUN apt-get upgrade -y
-;;  ---> Using cache
-;;  ---> cf78b3f1ba8e
-;; Step 8/15 : RUN apt-get install -y xpra
-;;  ---> Using cache
-;;  ---> abc6875920af
-;; Step 9/15 : RUN apt-get install -y python-dbus
-;;  ---> Using cache
-;;  ---> db04fe0b2fc4
-;; Step 10/15 : RUN adduser --disabled-password --gecos "" devbox
-;;  ---> Using cache
-;;  ---> 057184ad5f47
-;; Step 11/15 : RUN echo "devbox:asdf"| chpasswd
-;;  ---> Using cache
-;;  ---> c5c7a82198d2
-;; Step 12/15 : RUN useradd -G xpra vivek #add devbox user to xpra group
-;;  ---> Using cache
-;;  ---> 67f66bfdf776
-;; Step 13/15 : RUN mkdir /var/run/sshd
-;;  ---> Using cache
-;;  ---> 13dc930681f3
-;; Step 14/15 : EXPOSE 22
-;;  ---> Using cache
-;;  ---> bde467423411
-;; Step 15/15 : CMD /usr/sbin/sshd -D
-;;  ---> Using cache
-;;  ---> bc4cdc4b79a8
-;; Successfully built bc4cdc4b79a8
-;; SECURITY WARNING: You are building a Docker image from Windows against a non-Windows Docker host. All files and directories added to build context will have '-rwxr-xr-x' permissions. It is recommended to double check and reset permissions for sensitive files and directories.
+REM d:\devstation-workspace\projects\docker-devstation>docker build -t devbox .
+REM Sending build context to Docker daemon 77.31 kB
+REM Step 1/15 : FROM ubuntu:14.04
+REM  ---> b969ab9f929b
+REM Step 2/15 : MAINTAINER Pawel Panasewicz
+REM  ---> Using cache
+REM  ---> db5bfaab0e64
+REM Step 3/15 : RUN apt-get update -y  && apt-get upgrade -y  && apt-get install -y  apt-transport-https  curl  wget  telnet  vim  openssh-server  npm  firefox  sudo
+REM  ---> Using cache
+REM  ---> 644e239ec4fd
+REM Step 4/15 : RUN curl https://xpra.org/gpg.asc | apt-key add -
+REM  ---> Using cache
+REM  ---> fc011981f85c
+REM Step 5/15 : RUN echo "deb https://xpra.org/ trusty main" > /etc/apt/sources.list.d/xpra.list
+REM  ---> Using cache
+REM  ---> 571bc6da050f
+REM Step 6/15 : RUN apt-get update -y
+REM  ---> Using cache
+REM  ---> e3c3b061a492
+REM Step 7/15 : RUN apt-get upgrade -y
+REM  ---> Using cache
+REM  ---> cf78b3f1ba8e
+REM Step 8/15 : RUN apt-get install -y xpra
+REM  ---> Using cache
+REM  ---> abc6875920af
+REM Step 9/15 : RUN apt-get install -y python-dbus
+REM  ---> Using cache
+REM  ---> db04fe0b2fc4
+REM Step 10/15 : RUN adduser --disabled-password --gecos "" devbox
+REM  ---> Using cache
+REM  ---> 057184ad5f47
+REM Step 11/15 : RUN echo "devbox:asdf"| chpasswd
+REM  ---> Using cache
+REM  ---> c5c7a82198d2
+REM Step 12/15 : RUN useradd -G xpra vivek #add devbox user to xpra group
+REM  ---> Using cache
+REM  ---> 67f66bfdf776
+REM Step 13/15 : RUN mkdir /var/run/sshd
+REM  ---> Using cache
+REM  ---> 13dc930681f3
+REM Step 14/15 : EXPOSE 22
+REM  ---> Using cache
+REM  ---> bde467423411
+REM Step 15/15 : CMD /usr/sbin/sshd -D
+REM  ---> Using cache
+REM  ---> bc4cdc4b79a8
+REM Successfully built bc4cdc4b79a8
+REM SECURITY WARNING: You are building a Docker image from Windows against a non-Windows Docker host. All files and directories added to build context will have '-rwxr-xr-x' permissions. It is recommended to double check and reset permissions for sensitive files and directories.
--- 2-docker-run.bat
+++ 2-docker-run.bat
@@ -1,20 +1,20 @@
-;; Run it from windows CMD
+REM Run it from windows CMD
 
 docker run -d -p 127.0.0.1:22:22 devbox
 
 
-;; Output should be something like this:
-;; d:\devstation-workspace\projects\docker-devstation>docker run -d -p 127.0.0.1:22:22 devbox
-;; f7f90fc7aed8c14562c104bd3bbc591dc50a6b9b1d9ca245678b7197ed55ac67
+REM Output should be something like this:
+REM d:\devstation-workspace\projects\docker-devstation>docker run -d -p 127.0.0.1:22:22 devbox
+REM f7f90fc7aed8c14562c104bd3bbc591dc50a6b9b1d9ca245678b7197ed55ac67
 
-;;Optionally verify that that container is running:
+REM Optionally verify that that container is running:
 
 docker ps
 
-;;Output should be similar like this:
+REM Output should be similar like this:
 
-;; d:\devstation-workspace\projects\docker-devstation>docker ps
-;; CONTAINER ID        IMAGE               COMMAND               CREATED                  STATUS              PORTS                  NAMES
-;; f7f90fc7aed8        devbox              "/usr/sbin/sshd -D"   Less than a second ago   Up 41 seconds       127.0.0.1:22->22/tcp   zealous_morse
+REM d:\devstation-workspace\projects\docker-devstation>docker ps
+REM CONTAINER ID        IMAGE               COMMAND               CREATED                  STATUS              PORTS                  NAMES
+REM f7f90fc7aed8        devbox              "/usr/sbin/sshd -D"   Less than a second ago   Up 41 seconds       127.0.0.1:22->22/tcp   zealous_morse
 
 
